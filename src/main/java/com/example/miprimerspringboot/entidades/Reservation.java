1896f518cebb286cea376dc074f7ee2dba5386d4
juansebastianleyva <jnleyva@gmail.com>
2022-10-03 22:00:25 -0500

    Completo modelo, sin probar

--- src/main/java/com/example/miprimerspringboot/entidades/Reservation.java
+++ src/main/java/com/example/miprimerspringboot/entidades/Reservation.java
@@ -1,6 +1,8 @@
 package com.example.miprimerspringboot.entidades;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
@@ -16,8 +18,18 @@ public class Reservation implements Serializable {
     private Date devolutionDate;
     private String status;
 
+    @ManyToOne
+    @JoinColumn(name = "libId")
+    @JsonIgnoreProperties("reservations")
     private Library lib;
+
+    @ManyToOne
+    @JoinColumn(name = "clientId")
+    @JsonIgnoreProperties({"reservations","messages"})
     private Client client;
+
+    @OneToOne(cascade = {CascadeType.REMOVE},mappedBy="reservation")
+    @JsonIgnoreProperties("reservation")
     private Score score;
 
 

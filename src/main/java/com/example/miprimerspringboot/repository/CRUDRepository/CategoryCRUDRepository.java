eccf9a56dc8faa974c1283b9d0d8ae3566881994
Tatiana Suarez <tsuarez@bv.studio>
2022-10-04 20:53:14 -0500

    Category

--- src/main/java/com/example/miprimerspringboot/repository/CRUDRepository/CategoryCRUDRepository.java
+++ src/main/java/com/example/miprimerspringboot/repository/CRUDRepository/CategoryCRUDRepository.java
@@ -1,7 +1,8 @@
 package com.example.miprimerspringboot.repository.CRUDRepository;
 
+import com.example.miprimerspringboot.entidades.Category;
 import com.example.miprimerspringboot.entidades.Student;
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentCRUDRepository extends CrudRepository<Student,Integer> {
+public interface CategoryCRUDRepository extends CrudRepository<Category,Integer> {
 }
--- src/main/java/com/example/miprimerspringboot/repository/CategoryRepository.java
+++ src/main/java/com/example/miprimerspringboot/repository/CategoryRepository.java
@@ -1,2 +1,23 @@
-package com.example.miprimerspringboot.repository;public class CategoryRepository {
+package com.example.miprimerspringboot.repository;
+
+import com.example.miprimerspringboot.entidades.Category;
+import com.example.miprimerspringboot.repository.CRUDRepository.CategoryCRUDRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CategoryRepository {
+    @Autowired
+    private CategoryCRUDRepository categoryCRUDRepository;
+
+    public List<Category> getAll(){
+        return (List<Category>) categoryCRUDRepository.findAll();
+
+    }
+    public Category save (Category c) {
+       return categoryCRUDRepository.save(c);
+    }
+
 }
